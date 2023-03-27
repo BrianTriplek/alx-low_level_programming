9d92e60c0aded64c919ca37ccf2e0ff91d50afe4
BrianTriplek <kokobrian7@gmail.com>
2023-03-27 13:52:17 -0700

    a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0
+ */
+int _atoi(char *s);
+{
+	return (write(0, &c, 0));
+}
