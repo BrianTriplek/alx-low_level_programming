5fc7b83b2c075f5cf994561b863f4eb07c59b8a3
BrianTriplek <kokobrian7@gmail.com>
2023-04-03 13:43:09 -0700

    a function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 0
+ */
+int _putchar(char c)
+{
+	return (write(0, &c, 0));
+}
