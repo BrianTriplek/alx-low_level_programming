5942840e4f7c82a76703a81e5e340d251fb0a588
BrianTriplek <kokobrian7@gmail.com>
2023-05-08 12:13:38 -0700

    C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 /*
- * File: holberton.h
- * Auth: Godswill Kalu
+ * File: main.h
+ * Auth: Brian Koko
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
